dc242bbd97ef2da231cefe1e5286aa4b4c2844a9
Alejandro Sazo <alejandrosazog@gmail.com>
2016-11-03 11:28:30 -0300

    update script experimentos

--- tarea2/1_script.py
+++ tarea2/1_script.py
@@ -33,8 +33,8 @@ x_val, Y_val, y_val = val
 
 # Iterar sobre nuevas dimensionalidades
 d_prime = [2, 8, 32, 64]
-
-"""for d in d_prime:
+"""
+for d in d_prime:
     input_img = Input(shape=(784,))
     encoded = Dense(d, activation='sigmoid')(input_img)
     decoded = Dense(784, activation='sigmoid')(encoded)
@@ -46,12 +46,12 @@ d_prime = [2, 8, 32, 64]
     autoencoder.compile(optimizer=SGD(lr=1.0), loss='binary_crossentropy')
     hist = autoencoder.fit(x_train,x_train,nb_epoch=50,batch_size=25,shuffle=True,
     validation_data=(x_val, x_val))
-    autoencoder.save('1_1/basic_autoencoder_relu_768x'+str(d)+'.h5')
-    autoencoder.save('1_1/basic_encoder_relu_768x'+str(d)+'.h5')
-    autoencoder.save('1_1/basic_decoder_relu_768x'+str(d)+'.h5')"""
+    autoencoder.save('1_1/basic_autoencoder_sigmoid_768x'+str(d)+'.h5')
+    encoder.save('1_1/basic_encoder_sigmoid_768x'+str(d)+'.h5')
+    decoder.save('1_1/basic_decoder_sigmoid_768x'+str(d)+'.h5')
 
 # Pruebas con encoder relu y decoder sigmoid
-"""for d in d_prime:
+for d in d_prime:
     input_img = Input(shape=(784,))
     encoded = Dense(d, activation='relu')(input_img)
     decoded = Dense(784, activation='sigmoid')(encoded)
@@ -61,15 +61,15 @@ d_prime = [2, 8, 32, 64]
     decoder_layer = autoencoder.layers[-1]
     decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
     autoencoder.compile(optimizer=SGD(lr=1.0), loss='binary_crossentropy')
-    hist = autoencoder.fit(x_train,x_train,nb_epoch=50,batch_size=25,shuffle=True,
+    autoencoder.fit(x_train,x_train,nb_epoch=50,batch_size=25,shuffle=True,
     validation_data=(x_val, x_val))
     autoencoder.save('1_1/basic_autoencoder_relusig_768x'+str(d)+'.h5')
-    autoencoder.save('1_1/basic_encoder_relusig_768x'+str(d)+'.h5')
-    autoencoder.save('1_1/basic_decoder_relusig_768x'+str(d)+'.h5')"""
+    encoder.save('1_1/basic_encoder_relusig_768x'+str(d)+'.h5')
+    decoder.save('1_1/basic_decoder_relusig_768x'+str(d)+'.h5')
 
 
 # Pruebas con encoder sigmoid y decoder relu
-"""for d in d_prime:
+for d in d_prime:
     input_img = Input(shape=(784,))
     encoded = Dense(d, activation='sigmoid')(input_img)
     decoded = Dense(784, activation='relu')(encoded)
@@ -79,19 +79,20 @@ d_prime = [2, 8, 32, 64]
     decoder_layer = autoencoder.layers[-1]
     decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
     autoencoder.compile(optimizer=SGD(lr=1.0), loss='binary_crossentropy')
-    hist = autoencoder.fit(x_train,x_train,nb_epoch=50,batch_size=25,shuffle=True,
+    autoencoder.fit(x_train,x_train,nb_epoch=50,batch_size=25,shuffle=True,
     validation_data=(x_val, x_val))
     autoencoder.save('1_1/basic_autoencoder_sigrelu_768x'+str(d)+'.h5')
-    autoencoder.save('1_1/basic_encoder_sigrelu_768x'+str(d)+'.h5')
-    autoencoder.save('1_1/basic_decoder_sigrelu_768x'+str(d)+'.h5')"""
+    encoder.save('1_1/basic_encoder_sigrelu_768x'+str(d)+'.h5')
+    decoder.save('1_1/basic_decoder_sigrelu_768x'+str(d)+'.h5')
+"""
 
 # c) Calidad de representacion via kNN
 from sklearn.neighbors import KNeighborsClassifier
 from keras.models import load_model
 from timeit import default_timer as timer
 
-
-"""print "Resultado en data original"
+"""
+print "Resultado en data original"
 clf = KNeighborsClassifier(10)
 print "Fitting..."
 clf.fit(x_train, y_train)
@@ -100,8 +101,9 @@ print "Score..."
 score = clf.score(x_test, y_test)
 end = timer()
 print 'Classification Accuracy %.2f' % score
-print "Time: ", (end - start)"""
-"""
+print "Time: ", (end - start)
+
+
 print "Resultados con AE Sigmoid"
 for d in d_prime:
     autoencoder = load_model('1_1/basic_autoencoder_sigmoid_768x'+str(d)+'.h5')
@@ -147,8 +149,8 @@ def clustering_accuracy(pred_labels,y,nclusters=10):
 
 from sklearn.cluster import KMeans
 from sklearn import metrics
-
 """
+
 print "kmeans relusig"
 for d in d_prime:
     print "dim:",str(d)
@@ -161,9 +163,9 @@ for d in d_prime:
     labels_pred = model.fit_predict(encoded_train)
     score = metrics.adjusted_rand_score(y_train, labels_pred)
     print 'Clustering ARI %.2f' % score
-    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)"""
+    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)
 
-"""print "kmeans sigmoid"
+print "kmeans sigmoid"
 for d in d_prime:
     print "dim:",str(d)
     autoencoder = load_model('1_1/basic_autoencoder_sigmoid_768x'+str(d)+'.h5')
@@ -175,24 +177,24 @@ for d in d_prime:
     labels_pred = model.fit_predict(encoded_train)
     score = metrics.adjusted_rand_score(y_train, labels_pred)
     print 'Clustering ARI %.2f' % score
-    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)"""
-
-
-
+    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)
 """
-print "Raw data"
+
+
+
+"""print "Raw data"
 model = KMeans(n_clusters=10)
 labels_pred = model.fit_predict(x_train)
 score = metrics.adjusted_rand_score(y_train, labels_pred)
 print 'Clustering ARI %.2f' % score
 print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)"""
 
-
-"""## e) Analisis via PCA
+## e) Analisis via PCA
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 
 # Calidad de representacion
+"""
 for d in d_prime:
     print "Calidad representacion PCA d =",d
     pca = PCA(n_components=d)
@@ -209,8 +211,8 @@ for d in d_prime:
     labels_pred = model.fit_predict(pca_train)
     score = metrics.adjusted_rand_score(y_train, labels_pred)
     print 'Clustering ARI %.2f' % score
-    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred, y_train)"""
-
+    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred, y_train)
+"""
 
 ## f) Analisis via RBM
 from sklearn.neural_network import BernoulliRBM
@@ -230,6 +232,9 @@ import pickle
     rbm = pickle.load(open("1_1/basicRBM_"+str(d)+".pickle", "rb"))
     rbm_train = rbm.transform(x_train)
     rbm_test = rbm.transform(x_test)
+    print "Compressed dim:", float(rbm_test.shape[1])
+    print "Original dim:", float(x_train.shape[1])
+    print "Compresion:", float(x_train.shape[1])/float(rbm_test.shape[1])
     #print "Reconstruction error %.2f" % rbm.score(x_test)
     clf = KNeighborsClassifier(10)
     clf.fit(rbm_train, y_train)
@@ -242,4 +247,153 @@ import pickle
     print 'Clustering ARI %.2f' % score
     print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred, y_train)"""
 
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from keras.models import load_model
 
+
+## Modificacion de AE en Deep AE
+new_d_prime = [2,4,8,16,32]
+"""
+## Caso L = 2
+for d in new_d_prime:
+    target_dim = d #try other and do a nice plot
+    input_img = Input(shape=(784,))
+    encoded1 = Dense(1000, activation='relu')(input_img)
+    encoded2 = Dense(target_dim, activation='relu')(encoded1)
+    decoded2 = Dense(1000, activation='relu')(encoded2)
+    decoded1 = Dense(784, activation='sigmoid')(decoded2)
+    autoencoder = Model(input=input_img, output=decoded1)
+    encoder = Model(input=input_img, output=encoded1)
+    autoencoder.compile(optimizer=SGD(lr=1.0), loss='binary_crossentropy')
+    autoencoder.fit(x_train,x_train,nb_epoch=25,batch_size=25,shuffle=True, validation_data=(x_val, x_val))
+    autoencoder.save('my_autoencoder_784x1000x'+str(d)+'.h5')
+    encoder.save('my_encoder_784x1000x'+str(d)+'.h5')
+    pca = PCA(n_components=target_dim)
+    pca.fit(x_train)
+    print "Reconstruction error en d=",d,":",pca.score(x_test)
+
+
+## Caso L = 3
+for d in new_d_prime:
+    target_dim = d #try other and do a nice plot
+    input_img = Input(shape=(784,))
+    encoded1 = Dense(1000, activation='relu')(input_img)
+    encoded2 = Dense(500, activation='relu')(encoded1)
+    encoded3 = Dense(target_dim, activation='relu')(encoded2)
+    decoded3 = Dense(500, activation='relu')(encoded3)
+    decoded2 = Dense(1000, activation='relu')(decoded3)
+    decoded1 = Dense(784, activation='sigmoid')(decoded2)
+    autoencoder = Model(input=input_img, output=decoded1)
+    encoder = Model(input=input_img, output=encoded2)
+    autoencoder.compile(optimizer=SGD(lr=1.0), loss='binary_crossentropy')
+    autoencoder.fit(x_train,x_train,nb_epoch=25,batch_size=25,shuffle=True, validation_data=(x_val, x_val))
+    autoencoder.save('my_autoencoder_784x1000x500x'+str(d)+'.h5')
+    encoder.save('my_encoder_784x1000x500x'+str(d)+'.h5')
+    pca = PCA(n_components=target_dim)
+    pca.fit(x_train)
+    print "Reconstruction error en d=",d,":",pca.score(x_test)
+
+
+## Caso L = 4
+for d in new_d_prime:
+    target_dim = d #try other and do a nice plot
+    input_img = Input(shape=(784,))
+    encoded1 = Dense(1000, activation='relu')(input_img)
+    encoded2 = Dense(500, activation='relu')(encoded1)
+    encoded3 = Dense(250, activation='relu')(encoded2)
+    encoded4 = Dense(target_dim, activation='relu')(encoded3)
+    decoded4 = Dense(250, activation='relu')(encoded4)
+    decoded3 = Dense(500, activation='relu')(encoded3)
+    decoded2 = Dense(1000, activation='relu')(decoded3)
+    decoded1 = Dense(784, activation='sigmoid')(decoded2)
+    autoencoder = Model(input=input_img, output=decoded1)
+    encoder = Model(input=input_img, output=encoded3)
+    autoencoder.compile(optimizer=SGD(lr=1.0), loss='binary_crossentropy')
+    autoencoder.fit(x_train,x_train,nb_epoch=25,batch_size=25,shuffle=True, validation_data=(x_val, x_val))
+    autoencoder.save('my_autoencoder_784x1000x500x250x'+str(d)+'.h5')
+    encoder.save('my_encoder_784x1000x500x250x'+str(d)+'.h5')
+    pca = PCA(n_components=target_dim)
+    pca.fit(x_train)
+    print "Reconstruction error en d=",d,":",pca.score(x_test)
+"""
+
+new_d_prime = [8,16,32]
+for d in new_d_prime:
+    print "Calidad representacion dAE d =",d,"y L=2"
+    encoder = load_model('my_autoencoder_784x1000x'+str(d)+'.h5')
+    #encoder = load_model('my_encoder_784x1000x'+str(d)+'.h5')
+    print "Predicting"
+    encoded_train = encoder.predict(x_train)
+    encoded_test = encoder.predict(x_test)
+    clf = KNeighborsClassifier(10)
+    clf.fit(encoded_train, y_train)
+    score = clf.score(encoded_test, y_test)
+    print 'dAE kNN SCORE %.2f' % score
+    model = KMeans(n_clusters=10)
+    labels_pred = model.fit_predict(encoded_train)
+    score = metrics.adjusted_rand_score(y_train, labels_pred)
+    print 'Clustering ARI %.2f' % score
+    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)
+
+    print "Calidad representacion dAE d =",d,"y L=3"
+    dAE = load_model('my_autoencoder_784x1000x500x'+str(d)+'.h5')
+    #encoder = load_model('my_encoder_784x1000x500x'+str(d)+'.h5')
+    print "Predicting"
+    encoded_train = encoder.predict(x_train)
+    encoded_test = encoder.predict(x_test)
+    clf = KNeighborsClassifier(10)
+    clf.fit(encoded_train, y_train)
+    score = clf.score(encoded_test, y_test)
+    print 'dAE kNN SCORE %.2f' % score
+    model = KMeans(n_clusters=10)
+    labels_pred = model.fit_predict(encoded_train)
+    score = metrics.adjusted_rand_score(y_train, labels_pred)
+    print 'Clustering ARI %.2f' % score
+    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)
+
+
+    print "Calidad representacion dAE d =",d,"y L=4"
+    dAE = load_model('my_autoencoder_784x1000x500x250x'+str(d)+'.h5')
+    #encoder = load_model('my_encoder_784x1000x500x250x'+str(d)+'.h5')
+    print "Predicting"
+    encoded_train = encoder.predict(x_train)
+    encoded_test = encoder.predict(x_test)
+    clf = KNeighborsClassifier(10)
+    clf.fit(encoded_train, y_train)
+    score = clf.score(encoded_test, y_test)
+    print 'dAE kNN SCORE %.2f' % score
+    model = KMeans(n_clusters=10)
+    labels_pred = model.fit_predict(encoded_train)
+    score = metrics.adjusted_rand_score(y_train, labels_pred)
+    print 'Clustering ARI %.2f' % score
+    print 'Clustering ACC %.2f' % clustering_accuracy(labels_pred,y_train)
+
+
+
+"""
+Calidad representacion dAE d = 2 y L=2
+Predicting
+dAE kNN SCORE 0.65
+Clustering ARI 0.34
+Clustering ACC 0.56
+Calidad representacion dAE d = 2 y L=3
+Predicting
+dAE kNN SCORE 0.65
+Clustering ARI 0.32
+Clustering ACC 0.56
+Calidad representacion dAE d = 2 y L=4
+Predicting
+dAE kNN SCORE 0.65
+Clustering ARI 0.34
+Clustering ACC 0.56
+Calidad representacion dAE d = 4 y L=2
+Predicting
+dAE kNN SCORE 0.87
+Clustering ARI 0.39
+Clustering ACC 0.61
+Calidad representacion dAE d = 4 y L=3
+Predicting
+dAE kNN SCORE 0.87
+
+"""
